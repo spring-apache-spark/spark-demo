7ee4c2d1e9fe9c73388ac5dcf5cdd074c06b36c9
Pratap A K <pratap.ak@ge.com>
2019-10-18 11:58:25 +0530

    initial commit

---
+++ src/test/java/com/nsc/sparkdemo/SparkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsc.sparkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
